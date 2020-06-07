49b0c625af8efdd491e7427159ae0435d79228b7
ne1ro <neiro.mail@gmail.com>
2020-06-07 20:31:04 +0200

    Remove telegram & web parts from project
    As I want to use web app as a client and it needs to
    be re-done from scratch.

--- src/messenger/src/pl_stats_bot/use_cases.clj
+++
@@ -1,1 +0,0 @@
-(ns messenger.src.pl-stats-bot.use-cases)
--- src/pl_stats_bot/messenger.clj
+++
@@ -1,9 +0,0 @@
-(ns pl-stats-bot.messenger
-  "Messenger port")
-
-(defprotocol Messenger
-  "Messenger bot actions behaviour"
-
-  (set-endpoint [conf] "Sets an endpoint for the webhook")
-  (send-message [conf text chat-id] "Sends a message")
-  (send-photo [conf file chat-id] "Sends a photo"))
